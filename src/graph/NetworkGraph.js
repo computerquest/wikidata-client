53186657d04c6e1d88fe642abc72655d8d25b9eb
Jared Stigter <jaredstigter@gmail.com>
2020-03-16 23:41:13 -0700

    remade search into GraphController Made appropriate changes to network graph Got preliminary on onclick path coloring [needs improvement]

--- src/graph/NetworkGraph.js
+++ src/graph/NetworkGraph.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
-import IconButton from '@material-ui/core/Button';
-import PauseIcon from '@material-ui/icons/Pause';
-import PlayArrow from '@material-ui/icons/PlayArrow';
-import Search from '../search/Search.js'
+import GraphController from '../search/GraphController.js'
 
 function NetworkGraph(props) {
-	const [graph, setGraph] = React.useState({ nodes: [], links: [], checked: 0, frontier: 0 })
+	const [graph, setGraph] = React.useState({ nodes: [], links: [], checked: 0, frontier: 0, paths: [] })
 	const [loading, setLoading] = React.useState(false)
 	const [active, setActive] = React.useState(true)
 	const [target, setTarget] = React.useState([])
 	const [error, setError] = React.useState(false)
+	const [focusNode, setFocusNode] = React.useState('')
 
 	//does the graph polling
 	React.useEffect(() => {
@@ -75,6 +73,147 @@ function NetworkGraph(props) {
 		}
 	}, [active, target])
 
+	//this sets the color property for nodes not involved and darkens edges involved
+	React.useEffect(() => {
+		if (focusNode === '') {
+			return
+		}
+
+		console.log('doing the focus nnoe stuff')
+
+		const colorEdge = (start, end) => {
+			console.log('coloring', start, end)
+			let key = start + end
+			let link = -1
+			for (let i = 0; i < graph.links.length; i++) {
+				if (graph.links[i].id === key) {
+					link = i
+					break
+				}
+			}
+			if (link === -1) {
+				key = end + start
+				for (let i = 0; i < graph.links.length; i++) {
+					if (graph.links[i].id === key) {
+						link = i
+						break
+					}
+				}
+			}
+			console.log(link)
+			graph.links[link].color = 'red'
+		}
+
+		const paths = graph.paths
+		for (let i = 0; i < paths.length; i++) {
+			// const find = paths[i].findIndex((el) => el === focusNode)
+			// if (find !== -1) {
+			// 	if(find > 0) {
+			// 		colorEdge(paths[i][find], paths[i][find-1])
+			// 	}
+
+			// 	if(find < paths[i].length -1) {
+			// 		colorEdge(paths[i][find], paths[i][find +1])
+			// 	}
+			// }
+
+			if (paths[i].includes(focusNode)) {
+				for (let a = 0; a < paths[i].length - 1; a++) {
+					console.log(paths[i][a], i, a)
+					for (let b = 0; b < graph.nodes.length; b++) {
+						if (graph.nodes[b].id === paths[i][a]) {
+							console.log('colored node')
+							graph.nodes[b].color = 'red'
+							break
+						}
+					}
+					colorEdge(paths[i][a], paths[i][a + 1])
+				}
+
+				for (let b = 0; b < graph.nodes.length; b++) {
+					if (graph.nodes[b].id === paths[i][paths[i].length - 1]) {
+						console.log('colored node')
+						graph.nodes[b].color = 'red'
+						break
+					}
+				}
+			}
+		}
+
+		return () => {
+			console.log('clearing the coloring')
+			const clearEdge = (start, end) => {
+				console.log('coloring', start, end)
+				let key = start + end
+				let link = -1
+				for (let i = 0; i < graph.links.length; i++) {
+					if (graph.links[i].id === key) {
+						link = i
+						break
+					}
+				}
+				if (link === -1) {
+					key = end + start
+					for (let i = 0; i < graph.links.length; i++) {
+						if (graph.links[i].id === key) {
+							link = i
+							break
+						}
+					}
+				}
+				console.log(link)
+				delete graph.links[link].color
+			}
+
+			for (let i = 0; i < paths.length; i++) {
+				if (paths[i].includes(focusNode)) {
+					for (let a = 0; a < paths[i].length - 1; a++) {
+						for (let b = 0; b < graph.nodes.length; b++) {
+							if (graph.nodes[b].id === paths[i][a]) {
+								console.log('deleting', graph.nodes[b])
+								delete graph.nodes[b].color
+								break
+							}
+						}
+						clearEdge(paths[i][a], paths[i][a + 1])
+					}
+
+					for (let b = 0; b < graph.nodes.length; b++) {
+						if (graph.nodes[b].id === paths[i][paths[i].length - 1]) {
+							console.log('colored node')
+							delete graph.nodes[b].color
+							break
+						}
+					}
+				}
+			}
+
+		}
+	}, [focusNode, graph])
+
+	let drawNode = (node, ctx, globalScale) => {
+		const label = node.label;
+		const fontSize = 12 / globalScale;
+		ctx.font = `${fontSize}px Sans-Serif`;
+		const textWidth = ctx.measureText(label).width;
+		const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+		ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+		ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.fillStyle = node.color;
+		ctx.fillText(label, node.x, node.y);
+	}
+
+	// const activeNode = (node, event) => {
+	// 	setFocusNode(node.id)
+
+	// 	event.preventDefault()
+	// }
+
+
 	if (error) {
 		return (<div><p>There has been an error</p></div>);
 	}
@@ -89,12 +228,7 @@ function NetworkGraph(props) {
 	return (
 		<div>
 			<div>
-				<Search updateTarget={setTarget} />
-				<IconButton
-					onClick={() => { setActive(!active) }}
-					disabled={!loading}>
-					{active ? <PauseIcon /> : <PlayArrow />}
-				</IconButton>
+				<GraphController active={active} setTarget={setTarget} setActive={setActive} loading={loading} />
 				{error && <h3>There has been an error</h3>}
 			</div>
 			<div style={custom_style} >
@@ -103,22 +237,11 @@ function NetworkGraph(props) {
 					nodeLabel="label"
 					linkLabel="label"
 					nodeAutoColorBy="distance"
+					//linkAutoColorBy="label"
 					enableNodeDrag={false}
-					nodeCanvasObject={(node, ctx, globalScale) => {
-						const label = node.label;
-						const fontSize = 12 / globalScale;
-						ctx.font = `${fontSize}px Sans-Serif`;
-						const textWidth = ctx.measureText(label).width;
-						const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
-
-						ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-						ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-
-						ctx.textAlign = 'center';
-						ctx.textBaseline = 'middle';
-						ctx.fillStyle = node.color;
-						ctx.fillText(label, node.x, node.y);
-					}}
+					nodeCanvasObject={drawNode}
+					onNodeClick={(node, event) => setFocusNode(node.id)}
+					onBackgroundClick={() => setFocusNode('')}
 				/>
 			</div>
 		</div>
---
+++ src/search/GraphController.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import Searchfield from './Searchfield.js'
+import IconButton from '@material-ui/core/Button';
+import PauseIcon from '@material-ui/icons/Pause';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import Grid from '@material-ui/core/Grid';
+
+export default function GraphController(props) {
+	const [terms, setTerms] = React.useState({})
+
+	const handleSubmit = (event) => {
+		console.log('submissin is handled', terms)
+		props.setTarget([terms.first, terms.second])
+
+		event.preventDefault();
+	}
+
+	const handleTyping = (who, event) => {
+		let c = terms
+		c[who] = event
+		setTerms(c)
+	}
+
+
+	return (
+		<Grid container justify="flex-end" alignItems="center">
+			<Grid item >
+				<Grid container spacing={1} >
+					<Grid item xs={6}><Searchfield id='first' label={'First Object'} onChange={handleTyping} /></Grid>
+					<Grid item xs={6}><Searchfield id='second' label={'Second Object'} onChange={handleTyping} /></Grid>
+				</Grid>
+			</Grid>
+			<Grid item >
+				<Grid container>
+					<Grid item xs={12}><Button color="primary" type="submit" onClick={handleSubmit}>Search</Button></Grid>
+					<Grid item xs={12}><IconButton
+						onClick={() => props.setActive(!props.active)}
+						disabled={!props.loading}>
+						{props.active ? <PauseIcon /> : <PlayArrow />}
+					</IconButton></Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+}
