3c1a3af0a65a0fa548e8eb9810fb9468c55247b9
Jared Stigter <jaredstigter@gmail.com>
2020-03-17 20:30:06 -0700

    redid/improved the click coloring

--- src/graph/NetworkGraph.js
+++ src/graph/NetworkGraph.js
@@ -3,12 +3,12 @@ import ForceGraph2D from 'react-force-graph-2d';
 import GraphController from '../search/GraphController.js'
 
 function NetworkGraph(props) {
-	const [graph, setGraph] = React.useState({ nodes: [], links: [], checked: 0, frontier: 0, paths: [] })
 	const [loading, setLoading] = React.useState(false)
 	const [active, setActive] = React.useState(true)
 	const [target, setTarget] = React.useState([])
 	const [error, setError] = React.useState(false)
 	const [focusNode, setFocusNode] = React.useState('')
+	const [graph, setGraph] = React.useState({ nodes: [], links: [], checked: 0, frontier: 0, paths: [] }) //this is the graph to be rendered
 
 	//does the graph polling
 	React.useEffect(() => {
@@ -74,122 +74,85 @@ function NetworkGraph(props) {
 	}, [active, target])
 
 	//this sets the color property for nodes not involved and darkens edges involved
+	// eslint-disable-next-line
 	React.useEffect(() => {
 		if (focusNode === '') {
 			return
 		}
 
-		console.log('doing the focus nnoe stuff')
 
-		const colorEdge = (start, end) => {
-			console.log('coloring', start, end)
-			let key = start + end
-			let link = -1
-			for (let i = 0; i < graph.links.length; i++) {
-				if (graph.links[i].id === key) {
-					link = i
+		//these would be better as sets
+		let links = graph.links
+		let nodes = graph.nodes
+		let paths = graph.paths
+
+		let colored_links = []
+		let colored_nodes = []
+
+		//gets all of the things to be colored
+		for (let i = 0; i < paths.length; i++) {
+			if (paths[i].includes(focusNode)) {
+				for (let a = 0; a < paths[i].length - 1; a++) {
+					colored_nodes.push(paths[i][a])
+					colored_links.push(paths[i][a] + paths[i][a + 1])
+					colored_links.push(paths[i][a + 1] + paths[i][a])
+				}
+				colored_nodes.push(paths[i][paths[i].length - 1])
+			}
+		}
+
+		let actually_colored = []
+		for (let i = 0; i < nodes.length; i++) {
+			let index = false
+			for (let a = 0; a < colored_nodes.length; a++) {
+				if (nodes[i].id === colored_nodes[a]) {
+					index = true
 					break
 				}
 			}
-			if (link === -1) {
-				key = end + start
-				for (let i = 0; i < graph.links.length; i++) {
-					if (graph.links[i].id === key) {
-						link = i
-						break
-					}
-				}
-			}
-			console.log(link)
-			graph.links[link].color = 'red'
-		}
-
-		const paths = graph.paths
-		for (let i = 0; i < paths.length; i++) {
-			// const find = paths[i].findIndex((el) => el === focusNode)
-			// if (find !== -1) {
-			// 	if(find > 0) {
-			// 		colorEdge(paths[i][find], paths[i][find-1])
-			// 	}
-
-			// 	if(find < paths[i].length -1) {
-			// 		colorEdge(paths[i][find], paths[i][find +1])
-			// 	}
-			// }
-
-			if (paths[i].includes(focusNode)) {
-				for (let a = 0; a < paths[i].length - 1; a++) {
-					console.log(paths[i][a], i, a)
-					for (let b = 0; b < graph.nodes.length; b++) {
-						if (graph.nodes[b].id === paths[i][a]) {
-							console.log('colored node')
-							graph.nodes[b].color = 'red'
-							break
-						}
-					}
-					colorEdge(paths[i][a], paths[i][a + 1])
-				}
-
-				for (let b = 0; b < graph.nodes.length; b++) {
-					if (graph.nodes[b].id === paths[i][paths[i].length - 1]) {
-						console.log('colored node')
-						graph.nodes[b].color = 'red'
-						break
-					}
-				}
+			if (!index) {
+				nodes[i].last_color = nodes[i].color
+				nodes[i].color = '#d9d9d9'
+				actually_colored.push(nodes[i])
 			}
 		}
 
-		return () => {
-			console.log('clearing the coloring')
-			const clearEdge = (start, end) => {
-				console.log('coloring', start, end)
-				let key = start + end
-				let link = -1
-				for (let i = 0; i < graph.links.length; i++) {
-					if (graph.links[i].id === key) {
-						link = i
-						break
-					}
-				}
-				if (link === -1) {
-					key = end + start
-					for (let i = 0; i < graph.links.length; i++) {
-						if (graph.links[i].id === key) {
-							link = i
-							break
-						}
-					}
-				}
-				console.log(link)
-				delete graph.links[link].color
-			}
+		colored_nodes = actually_colored
 
-			for (let i = 0; i < paths.length; i++) {
-				if (paths[i].includes(focusNode)) {
-					for (let a = 0; a < paths[i].length - 1; a++) {
-						for (let b = 0; b < graph.nodes.length; b++) {
-							if (graph.nodes[b].id === paths[i][a]) {
-								console.log('deleting', graph.nodes[b])
-								delete graph.nodes[b].color
-								break
-							}
-						}
-						clearEdge(paths[i][a], paths[i][a + 1])
-					}
-
-					for (let b = 0; b < graph.nodes.length; b++) {
-						if (graph.nodes[b].id === paths[i][paths[i].length - 1]) {
-							console.log('colored node')
-							delete graph.nodes[b].color
-							break
-						}
-					}
+		for (let i = 0; i < links.length; i++) {
+			let index = -1
+			for (let a = 0; a < colored_links.length; a++) {
+				if (links[i].id === colored_links[a]) {
+					index = a
+					colored_links[a] = links[i]
+					break
 				}
 			}
-
+			if (index !== -1) {
+				colored_links[index].color = '#666666'
+			}
 		}
-	}, [focusNode, graph])
+
+		//console.log(graph, rawGraph)
+		//everyting passed in should be from graph 
+		setGraph(g => g)
+
+		//this is how to undo this effect
+		return (() => {
+
+			for (let i = 0; i < colored_nodes.length; i++) {
+				colored_nodes[i].color = colored_nodes[i].last_color
+			}
+
+			for (let i = 0; i < colored_links.length; i++) {
+				delete colored_links[i].color
+
+			}
+
+			// eslint-disable-next-line
+			setGraph(g => g)
+		})
+	}, [focusNode])
 
 	let drawNode = (node, ctx, globalScale) => {
 		const label = node.label;
@@ -206,13 +169,6 @@ function NetworkGraph(props) {
 		ctx.fillStyle = node.color;
 		ctx.fillText(label, node.x, node.y);
 	}
-
-	// const activeNode = (node, event) => {
-	// 	setFocusNode(node.id)
-
-	// 	event.preventDefault()
-	// }
-
 
 	if (error) {
 		return (<div><p>There has been an error</p></div>);
@@ -237,7 +193,7 @@ function NetworkGraph(props) {
 					nodeLabel="label"
 					linkLabel="label"
 					nodeAutoColorBy="distance"
-					//linkAutoColorBy="label"
+					//nodeVisibility="visibility"
 					enableNodeDrag={false}
 					nodeCanvasObject={drawNode}
 					onNodeClick={(node, event) => setFocusNode(node.id)}
